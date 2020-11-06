305996cbd332ab83633e6b954087c3ddfbeecc05
davidmolina2810 <davidmolina2810@gmail.com>
2020-11-06 14:23:04 -0600

    Finished.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name].blank?
+      redirect_to login_path
+    else
+      session[:name] ||= params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.delete :name
+      redirect_to root_path
+    end
+  end
+
+end
